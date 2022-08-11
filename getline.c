b5aa329b19eb79f9b3991010cd58df9d51d6a88d
mezaya <mesaya@live.com>
2022-08-11 07:46:24 +0200

    Fixing

--- getline.c
+++ getline.c
@@ -57,7 +57,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 /**
  * assign_lineptr - Reassigns the lineptr variable for _getline.
- * @lineptr: A buffer to store an input string.
+ * @lineptr: A buffer to store an input string from the user.
  * @n: The size of lineptr.
  * @buffer: The string to assign to lineptr.
  * @b: The size of buffer.
@@ -88,7 +88,7 @@ void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
 }
 
 /**
- * _getline - Reads input from a stream.
+ * _getline - Reads input from a stream either file or user input.
  * @lineptr: A buffer to store the input.
  * @n: The size of lineptr.
  * @stream: The stream to read from.
